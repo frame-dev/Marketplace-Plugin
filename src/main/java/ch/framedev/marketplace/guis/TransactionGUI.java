0931343ce726fbbd9c1217384fbdf8d81d7518c9
framedev <darrylhuber39@gmail.com>
2025-04-22 19:25:10 +0200

    TransactionGUI added

--- src/main/java/ch/framedev/marketplace/guis/TransactionGUI.java
+++ src/main/java/ch/framedev/marketplace/guis/TransactionGUI.java
@@ -5,7 +5,7 @@ package ch.framedev.marketplace.guis;
 /*
  * ch.framedev.marketplace.guis
  * =============================================
- * This File was Created by FrameDev
+ * This File was Created by FrameDev.
  * Please do not change anything without my consent!
  * =============================================
  * This Class was created at 18.04.2025 17:35
